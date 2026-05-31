8dd61597710706fdf8fa7e23ff62ed3b2d78642d
anwar <anwarfelicienspringall@gmail.com>
2026-05-31 18:58:11 +0100

    Handlers have been made for the delete and view methods, minimal comments have been added to give brief descriptions of each method.

--- src/DbManager.java
+++ src/DbManager.java
@@ -1,7 +1,8 @@
 import java.sql.*;
+import java.util.HashMap;
 
 public class DbManager {
-
+// This method connects the backend with the SQLite file.
     public static Connection connect() {
 
         String url = "jdbc:sqlite:resources/CalendarTodos.db";
@@ -13,7 +14,7 @@ public class DbManager {
             return null;
         }
     }
-
+// This method queries the database and creates our allTodos table if it doesn't exist already.
    void createTable() {
         String sql = "Create table if not exists allTodos(" +
                 "todoId Integer Primary key," +
@@ -30,7 +31,7 @@ public class DbManager {
             System.out.println(e.getMessage());
         }
    }
-
+// This method queries the database and creates a todo with the corresponding Todo object attributes.
     void handleCreateTodo(Todo todo) {
         String sql = "INSERT into allTodos(todoDescription, todoDate, todoComplete) values(?,?,?)";
         try (Connection con = connect();
@@ -44,10 +45,38 @@ public class DbManager {
             System.out.println(e.getMessage());
         }
     }
+// This method queries the database and deletes a todo with a corresponding todoId.
+// Note: all ID attributes are stored as Strings.
+    void handleDeleteTodo(String todoId) {
+        String sql = "DELETE FROM allTodos WHERE todoId = ?";
+        try (Connection con = connect();
+            PreparedStatement pstmt = con.prepareStatement(sql)) {
 
+            pstmt.setString(1, todoId);
+            pstmt.executeUpdate();
 
-//fix
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+// This method queries the Todos Database and returns a hashmap of Strings and Todo Classes.
+    HashMap<String, Todo> handleViewTodo() {
+        String sql = "Select * from allTodos";
 
+        try (Connection con = connect();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+        ){
+            HashMap<String, Todo> todos = new HashMap<>();
+            while (rs.next()) {
+                Todo currTodo = new Todo(rs.getString("todoDescription"), rs.getString("todoDate"), rs.getBoolean("todoDate"));
+                todos.put(rs.getString("todoId"), currTodo);
+            }
+            return todos;
 
-
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -18,7 +20,9 @@ public class Main {
 
     void main_menu () {
         System.out.println("Welcome");
-        System.out.println("    -Create todo");
+        System.out.println("    -(1)Create todo");
+        System.out.println("    -(2)Delete todo");
+        System.out.println("    -(3)View todos");
 
         Scanner myInput = new Scanner(System.in);
         try {
@@ -33,6 +37,10 @@ public class Main {
                 case 2:
                     System.out.println("Delete a todo");
                     deleteTodo();
+                    break;
+                case 3:
+                    System.out.println("View Todos");
+                    viewTodo();
                     break;
                 default:
                     System.out.println("Invalid menu choice.");
@@ -60,11 +68,18 @@ public class Main {
     void deleteTodo(){
         try (Scanner input = new Scanner(System.in)){
             String todoId = input.nextLine();
+            dbManager.handleDeleteTodo(todoId);
         } catch (Exception e) {
             System.out.println("Invalid input");
         }
     }
 
-
-
+    void viewTodo() {
+        HashMap<String, Todo> todos = dbManager.handleViewTodo();
+        for (String todoId : todos.keySet()) {
+            System.out.println("    -" + todoId + ": " + todos.get(todoId).description);
+            System.out.println("     To be completed by: " + todos.get(todoId).date);
+            System.out.println("     Status: " +  ((todos.get(todoId).complete) ? "Completed" : "Incompleted"));
+        }
+    }
 }
